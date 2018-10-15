2250dc76b797f703c95834eb8b7ca54693a629cb
Gonlos <gonzalo.cortazar.kao@gmail.com>
2018-10-15 17:07:29 +0200

    Linck Back or profile in Header

--- src/page/Header.jsx
+++ src/page/Header.jsx
@@ -1,5 +1,9 @@
 import React from "react";
-const Header = () => {
-  return <header>Header{process.env.REACT_APP_PRUEBA}</header>;
+import './Header.scss'
+import {Link} from 'react-router-dom'
+const Header = (props) => {
+  const {pathname} = props.location
+  console.log(pathname)
+  return <header><Link to={pathname==="/"?"/profile":"/"}>{pathname==="/"?"profile":"back"}</Link>Header{process.env.REACT_APP_PRUEBA}</header>;
 };
 export default Header;
