6f51b2b39de33518e3f5fa7546c070bb21c35ab7
Gonlos <gonzalo.cortazar.kao@gmail.com>
2018-10-19 12:26:10 +0200

    Updates

---
+++ precache-manifest.1327def512fe2c34fdea91d5e2154867.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/client/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4834834ad1fe1d1d00a4b9fc776e3762",
+    "url": "/client/static/media/loading.4834834a.png"
+  },
+  {
+    "revision": "dd2da1d8f9d3944efe2797e1fa02e096",
+    "url": "/client/static/media/Quicksand_Book.dd2da1d8.otf"
+  },
+  {
+    "revision": "dad60270bf4017963b16",
+    "url": "/client/static/js/runtime~main.dad60270.js"
+  },
+  {
+    "revision": "dfc07743c4d72fc78096",
+    "url": "/client/static/js/main.dfc07743.chunk.js"
+  },
+  {
+    "revision": "986893ddcd82f2749fc4",
+    "url": "/client/static/js/1.986893dd.chunk.js"
+  },
+  {
+    "revision": "dfc07743c4d72fc78096",
+    "url": "/client/static/css/main.9874cf7a.chunk.css"
+  },
+  {
+    "revision": "ecbe857663f9d2d1df7b0fc419cd1ec9",
+    "url": "/client/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c8beb303b0d714ac33ed9b75c056e4c8.js"
+  "/client/precache-manifest.1327def512fe2c34fdea91d5e2154867.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/client/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
